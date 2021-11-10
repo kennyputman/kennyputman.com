cef9551810cc0520360f9fb2be56b3bd28a2d891
Kenny Putman <kbputman@gmail.com>
2021-11-10 17:37:25 -0600

    add product card

--- components/FeaturedProjects.tsx
+++ components/FeaturedProjects.tsx
@@ -1,16 +1,29 @@
 import ProjectCard from "./ProjectCard";
 
+const projectData = {
+  title: "Product Feedback Page",
+  description:
+    "Forum for taking in user feedback and prioritizing feature requests",
+  technologies: ["React", "Typescript", "GraphQL"],
+};
+
 const FeaturedProjects = () => {
   return (
-    <div>
-      <h2 className="text-3xl font-semibold mx-auto ">
-        Projects
-        <span className="w-10 border-b-2 "></span>
-      </h2>
-      <p className="mb-4 dark:text-gray-dark-soft text-gray-light-soft">
-        Some things I have been working on
-      </p>
-      <ProjectCard></ProjectCard>
+    <div className="mb-32">
+      <div className="mb-16">
+        <h2 className="text-3xl font-semibold mx-auto ">
+          Projects
+          <span className="w-10 border-b-2 "></span>
+        </h2>
+        <p className=" dark:text-gray-dark-soft text-gray-light-soft">
+          Some things I have been working on
+        </p>
+      </div>
+      <ProjectCard
+        title={projectData.title}
+        description={projectData.description}
+        technologies={projectData.technologies}
+      ></ProjectCard>
     </div>
   );
 };
--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,24 +1,31 @@
 import Image from "next/image";
 
-const ProjectCard = () => {
+type Props = {
+  title: string;
+  description: string;
+  technologies: string[];
+};
+
+const ProjectCard = ({ title, description, technologies }: Props) => {
+  console.log(title);
   return (
-    <div className="flex">
+    <div className="flex dark:bg-fx-dark-second rounded-md p-4">
       {/* Project GIF */}
       <div className="flex-1">
         <Image src="/logos/graphql.svg" alt="" height={250} width={250} />
       </div>
       {/* Project Description */}
       <div className="flex-1">
-        <h3>Product Feedback Page</h3>
-        <div>
-          <p>
-            Forum for taking in user feedback and prioritizing features requests
-          </p>
+        <h3 className="text-center text-2xl font-semibold p-2">{title}</h3>
+        <div className="p-4 ">
+          <p>{description}</p>
         </div>
-        <div className="flex dark:to-txt-emph gap-1">
-          <p>React</p>
-          <p>Typescript</p>
-          <p>GraphQL</p>
+        <div className="flex justify-center gap-4">
+          {technologies.map((tech) => (
+            <p key={tech} className="dark:text-txt-emph font-semibold">
+              {tech}
+            </p>
+          ))}
         </div>
       </div>
     </div>
